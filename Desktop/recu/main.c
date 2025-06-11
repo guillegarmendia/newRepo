53cefa0844e9275cfc14fed72fe946c19473d901
Guillem Garmendia Balmaña <guillem.garmendia@students.salle.url.edu>
2025-06-11 18:41:33 +0200

    first commit

---
+++ Desktop/recu/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    int option;
+    printf("----------------------");
+    printf("--LS ELECTRONICS HUB--");
+    printf("----------------------");
+
+    printf("\t\t1. Register\t\t2. Staff Access\t\t3. Client Login");
+
+    printf("Option: ");
+    scanf("%d", &option);
+
+    switch (option) {
+        case 1:
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        default:
+            printf("Invalid option");
+            break;
+    }
+    return 0;
+}
