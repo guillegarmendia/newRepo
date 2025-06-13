d538aefc9c9b3f09bb189ae398895d6d224fdf4a
Guillem Garmendia <guillem.garmendia@students.salle.url.edu>
2025-06-14 01:58:50 +0200

    TT2.1

--- Desktop/recu/Register.c
+++ Desktop/recu/Register.c
@@ -13,24 +13,56 @@ void registerShop() {
         return;
     }
 
-    printf("Shop's name: \n");
+    printf("Shop's name: ");
     fgets(shop.shop, sizeof(shop.shop), stdin);
     shop.shop[strlen(shop.shop)-1] = '\0';
 
 
-    printf("Shop's address: \n");
+    printf("Shop's address: ");
     fgets(shop.adress, sizeof(shop.adress), stdin);
     shop.adress[strlen(shop.adress)-1] = '\0';
 
-    printf("Shop's phone: \n");
+    printf("Shop's phone: ");
     scanf("%d", &shop.phone);
     getchar();
 
-    printf("Shop's mail: \n");
+    printf("Shop's mail: ");
     fgets(shop.mail, sizeof(shop.mail), stdin);
     shop.mail[strlen(shop.mail)-1] = '\0';
 
     fprintf(fp, "%s;%s;%d;%s\n", shop.shop, shop.adress, shop.phone, shop.mail);
+
+    fclose(fp);
+
+}
+
+
+void registerWorker() {
+
+    Shop shop;
+    FILE* fp = fopen("Workers.txt","a");
+
+    if (fp == NULL) {
+        printf("Error opening Workers.txt\n");
+        return;
+    }
+
+    printf("Name: ");
+    fgets(shop.worker, sizeof(shop.worker), stdin);
+    shop.worker[strlen(shop.worker)-1] = '\0';
+
+
+    printf("Mail: ");
+    fgets(shop.mail, sizeof(shop.mail), stdin);
+    shop.mail[strlen(shop.mail)-1] = '\0';
+
+    printf("Password: ");
+    fgets(shop.password, sizeof(shop.password), stdin);
+    shop.password[strlen(shop.password)-1] = '\0';
+
+    printf("Shop: \n");
+    fgets(shop.shop, sizeof(shop.shop), stdin);
+    shop.shop[strlen(shop.shop)-1] = '\0';
 
     fclose(fp);
 
--- Desktop/recu/Register.h
+++ Desktop/recu/Register.h
@@ -1,11 +1,16 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
+#define MAX 100
+
 typedef struct {
-    char shop[100];
-    char adress[100];
+    char shop[MAX];
+    char adress[MAX];
     int phone;
-    char mail[100];
+    char mail[MAX];
+    char worker[MAX];
+    char password[MAX];
+
 } Shop;
 
 void registerShop();
