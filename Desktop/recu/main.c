6cf1f14f4f25d1c8052a196f6bb94043bac9586d
Guillem Garmendia Balmaña <guillem.garmendia@students.salle.url.edu>
2025-06-14 01:32:32 +0200

    S1.Registrar botiga

--- Desktop/recu/main.c
+++ Desktop/recu/main.c
@@ -1,30 +1,77 @@
 #include <stdio.h>
+#include "Register.h"
+
+void showRegisterMenu() {
+    printf("\n\t\t1.1 Register Shop");
+    printf("\n\t\t1.2 Register Staff Member");
+    printf("\n\t\t1.3 Register Customer");
+    printf("\n\t\t1.4 Back");
+    printf("\nOption: ");
+}
+
+void showMainMenu() {
+    printf("\n\t\t1. Register\n\t\t2. Staff Access\n\t\t3. Client Login\n\t\t4. Exit");
+    printf("\nOption: ");
+}
 
 int main() {
 
-    int option;
-    printf("----------------------");
-    printf("--LS ELECTRONICS HUB--");
-    printf("----------------------");
+    printf("\n----------------------\n");
+    printf("--LS ELECTRONICS HUB--\n");
+    printf("----------------------\n");
 
-    printf("\t\t1. Register\t\t2. Staff Access\t\t3. Client Login");
+    int option = 0;
+    int registerOption = 0;
 
-    printf("Option: ");
-    scanf("%d", &option);
+    do {
+        showMainMenu();
+        scanf("%d", &option);
+        getchar();
 
-    switch (option) {
-        case 1:
-            break;
+        switch (option) {
+            case 1:
+                do {
+                    showRegisterMenu();
+                    scanf("%d", &registerOption);
+                    getchar();
 
-        case 2:
-            break;
+                    switch (registerOption) {
+                        case 1:
+                            registerShop();
+                            break;
+                        case 2:
 
-        case 3:
-            break;
+                            break;
+                        case 3:
 
-        default:
-            printf("Invalid option");
-            break;
-    }
+                            break;
+                        case 4:
+                            printf("Returning to Menu\n");
+                            break;
+                        default:
+                            printf("Invalid option\n");
+                            break;
+                    }
+                } while (registerOption != 4);
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+                printf("End of program!\n");
+                break;
+
+            default:
+                printf("Invalid option\n");
+                break;
+        }
+    } while (option != 4);
+
     return 0;
 }
