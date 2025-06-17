a4310183ce2a746aa2372224de508357f9d1628c
Guillem Garmendia <guillem.garmendia@students.salle.url.edu>
2025-06-17 21:28:16 +0200

    TT34.1

---
+++ Desktop/recu/Client.h
@@ -0,0 +1,13 @@
+
+#define MAX 100
+
+typedef struct {
+    char name[MAX];
+    char mail[MAX];
+    char password[MAX];
+    char shop[MAX];
+    char clientProduct[MAX];
+} Client;
+
+
+void clientSegmentation();
